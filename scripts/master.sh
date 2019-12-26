a8115527eda245e5b34d0f9f3c8776f482115f28
GKruglov <neurocyton@gmail.com>
2019-12-26 15:08:25 +0300

    deleted commented lines

--- scripts/master.sh
+++ scripts/master.sh
@@ -15,12 +15,6 @@ echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> SETUP
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y apt-transport-https unzip vim tmux software-properties-common curl wget rsync git python-pip ansible
 
-#echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> SETUP KUBECTL"
-#curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-#echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-#sudo apt-get update
-#sudo apt-get install -y kubectl
-
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> SETUP KUBESPRAY"
 cd ~
 git clone https://github.com/kubernetes-sigs/kubespray.git
